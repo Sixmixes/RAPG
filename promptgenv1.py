3618b30e164cb7437ba09c7ff577c0e5195bbb3a
Mike <106796005+Sixmixes@users.noreply.github.com>
2023-01-23 18:25:58 -0600

    adds while loop for user input

--- promptgenv1.py
+++ promptgenv1.py
@@ -6,3 +6,20 @@ settings = ["in a castle", "in a forest", "on a mountain", "in a cave"]
 adjectives = ["fierce", "beautiful", "mysterious", "ancient"]
 styles = ["abstract", "realistic", "surreal", "fantasy"]
 genmetas = ["dark", "light", "colorful", "muted"]
+
+# Number of styles and qualities to include in the prompt
+numstyles = 2
+numquality = 1
+
+while True:
+    user_input = input("Press enter to generate a new prompt, or enter a number 1-6 to update the corresponding list in the previous prompt: ")
+    if user_input == "":
+        # Select random elements from the lists
+        character = rn.choice(characters)
+        obj = rn.choice(objects)
+        setting = rn.choice(settings)
+        adj = rn.choice(adjectives)
+        liststyles = rn.sample(styles, numstyles) + rn.sample(genmetas, numquality)
+
+        # Create the prompt
+        prompt = character + ' ' + 'with a ' + obj + ' ' + setting + ', ' + adj + ' ' + ' '.join(liststyles)
