c742e6c602cdb3a237976f1bf1c33d295d5eb1cc
Mike <106796005+Sixmixes@users.noreply.github.com>
2023-01-23 18:31:24 -0600

    Print the prompt

--- promptgenv1.py
+++ promptgenv1.py
@@ -32,3 +32,38 @@ while True:
         prev_adjectives = adj
         prev_styles = liststyles
         prev_genmetas = genmetas
+
+        # Print the prompt
+        print("Prompt: " + prompt)
+    elif user_input == "1":
+        prev_characters = rn.choice(characters)
+        prev_prompt = prev_characters + ' ' + 'with a ' + prev_objects + ' ' + prev_settings + ', ' + prev_adjectives + ' ' + ' '.join(prev_styles)
+        print("The characters in the previous prompt has been updated to: ",prev_characters)
+        print("The updated prompt is: ",prev_prompt)
+    elif user_input == "2":
+        prev_objects = rn.choice(objects)
+        prev_prompt = prev_characters + ' ' + 'with a ' + prev_objects + ' ' + prev_settings + ', ' + prev_adjectives + ' ' + ' '.join(prev_styles)
+        print("The objects in the previous prompt has been updated to: ",prev_objects)
+        print("The updated prompt is: ",prev_prompt)
+    elif user_input == "3":
+        prev_settings = rn.choice(settings)
+        prev_prompt = prev_characters + ' ' + 'with a ' + prev_objects + ' ' + prev_settings + ', ' + prev_adjectives + ' ' + ' '.join(prev_styles)
+        print("The settings in the previous prompt has been updated to: ",prev_settings)
+        print("The updated prompt is: ",prev_prompt)
+    elif user_input == "4":
+        prev_adjectives = rn.choice(adjectives)
+        prev_prompt = prev_characters + ' ' + 'with a ' + prev_objects + ' ' + prev_settings + ', ' + prev_adjectives + ' ' + ' '.join(prev_styles)
+        print("The adjectives in the previous prompt has been updated to: ",prev_adjectives)
+        print("The updated prompt is: ",prev_prompt)
+    elif user_input == "5":
+        prev_styles = rn.choice(styles)
+        prev_prompt = prev_characters + ' ' + 'with a ' + prev_objects + ' ' + prev_settings + ', ' + prev_adjectives + ' ' + ' '.join(prev_styles)
+        print("The styles in the previous prompt has been updated to: ",prev_styles)
+        print("The updated prompt is: ",prev_prompt)
+    elif user_input == "6":
+        prev_genmetas = rn.choice(genmetas)
+        prev_prompt = prev_characters + ' ' + 'with a ' + prev_objects + ' ' + prev_settings + ', ' + prev_adjectives + ' ' + ' '.join(prev_styles)
+        print("The genmetas in the previous prompt has been updated to: ",prev_genmetas)
+        print("The updated prompt is: ",prev_prompt)
+    else:
+        print("Invalid input. Please enter a number 1-6 or press enter to generate a new prompt.")
