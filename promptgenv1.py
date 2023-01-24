3381a2f83046b8f6e3ae47f022bdc127f4507431
Mike <106796005+Sixmixes@users.noreply.github.com>
2023-01-23 18:09:25 -0600

    imports random as rn, declares variables for items to be in prompt

---
+++ promptgenv1.py
@@ -0,0 +1,8 @@
+import random as rn
+
+characters = ["a knight", "a dragon", "a princess", "a wizard"]
+objects = ["sword", "shield", "magic wand", "treasure chest"]
+settings = ["in a castle", "in a forest", "on a mountain", "in a cave"]
+adjectives = ["fierce", "beautiful", "mysterious", "ancient"]
+styles = ["abstract", "realistic", "surreal", "fantasy"]
+genmetas = ["dark", "light", "colorful", "muted"]
